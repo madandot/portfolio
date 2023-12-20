46cbd865acdbba213e13d6c9fa9039a58fa868f7
madandot <reddymadan0512@gmail.com>
2023-12-20 15:19:23 +0530

    initial commit

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -5,7 +5,7 @@ import { CiSaveDown1 } from "react-icons/ci";
 
 const Resume = () => {
    const handleDownload = () => {
-      const fileUrl = "https://drive.google.com/file/d/1DkIIkKI_BRRXNhfZVRkr-FmPkkpjYWcV/view?usp=sharing";
+      const fileUrl = "https://drive.google.com/file/d/1LDK-wV2BeW5o9U7J4SHEqsWODEX8w0SJ/view?usp=sharing";
       const link = document.createElement("a");
       link.href = fileUrl;
       link.target = "_blank"; // Set the target to _blank to open in a new tab
@@ -42,20 +42,25 @@ const Resume = () => {
                   <div className='py-2'>
                      <h1 className='text-xl font-bold pb-2'>Objective :</h1>
                      <p>
-                        Dedicated and enthusiastic BCA graduate with a passion for front-end development. Eager to leverage my skills in
-                        <b> HTML,</b> <b>CSS,</b>
-                        <b> Tailwind Css,</b> <b>JavaScript</b> and <b>React.js</b> to contribute to dynamic and user-friendly web
-                        applications.
+                        Passionate BCA graduate with a strong focus on front-end development, seeking opportunities to apply expertise in
+                        HTML, CSS, Tailwind CSS, JavaScript, and React.js. Dedicated to crafting seamless and innovative user experiences, I
+                        aim to contribute to the creation of dynamic and user-friendly web applications, leveraging my skills in responsive
+                        design and UI/UX principles.
                      </p>
                   </div>
                   <div className='py-2'>
                      <h1 className='text-xl font-bold pb-2'>Education :</h1>
-                     <p>
-                        <b>SSLC</b> <br /> Sahana English Medium High School <br /> Kotagudda 2016 <br /> <b>Pre-University Course</b>{" "}
-                        <br /> BGS PU College <br />
-                        Chikkaballapur 2018 <br /> <b>Bachelor of Computer Applications (BCA)</b> <br /> Administrative Management College{" "}
-                        <br /> Bangalore 2023
-                     </p>
+                     <div>
+                        <div>
+                           SSLC <br /> Sahana English Medium High School <br /> Kotagudda <br /> April 2016
+                        </div>
+                        <div>
+                           Pre-University Course <br /> BGS PU College <br /> Chikkaballapur <br /> March 2018
+                        </div>
+                        <div>
+                           Bachelor of Computer Applications (BCA) <br /> Administrative Management College <br /> Bangalore <br /> Aug 2023
+                        </div>
+                     </div>
                   </div>
                   <div className='py-2'>
                      <h1 className='text-xl font-bold pb-2'>Technical Skills :</h1>
@@ -69,27 +74,29 @@ const Resume = () => {
                   <div className='py-2'>
                      <h1 className='text-xl font-bold pb-2'>Projects :</h1>
                      <p>
-                        1. <b className='text-[#00fe87] cursor-pointer'>FILMPLEX</b> |{" "}
-                        <b> Javascript, React, Tailwindcss, scss, React-redux, Vite</b> <br /> • Developed a website using The Movie
-                        Database (TMDb) to serve as a REST API with React as the frontend. <br /> • Implemented global search functionality
-                        that allows users to search for TV shows and movies. <br /> 2.{" "}
-                        <b className='text-[#00fe87] cursor-pointer'>LIGHT STUDIO</b> |{" "}
-                        <b>Javascript, Typescript, React, TailwindCss, React-Redux, Next js</b> <br /> • Implemented an items listing page
-                        that enables users to filter and sort items in various ways. Users can also add items to their cart. <br /> •
-                        Implemented an individual item page that displays all the information related to a specific item. <br />
+                        1. <b className='text-[#00fe87] cursor-pointer'>FILMPLEX</b> <br />• Developed a robust website utilizing TMDB's
+                        REST API with React for the frontend, enhancing user interaction and experience. <br />• Implemented global search
+                        functionality, empowering users to discover TV shows and movies effortlessly. <br />• Technologies: JavaScript,
+                        React, TMDB API, React-Redux, Tailwind CSS. <br />
+                        2. <b className='text-[#00fe87] cursor-pointer'> LIGHT STUDIO</b> <br />• Engineered an intuitive item listing
+                        platform with advanced filtering and sorting capabilities, optimizing user navigation. <br />• Created
+                        individualized item pages showcasing comprehensive details, enhancing user engagement and decision-making. <br />•
+                        Technologies: JavaScript, TypeScript, React, Next.js, React-Redux, Tailwind CSS.
                      </p>
                   </div>
                   <div className='py-2'>
                      <h1 className='text-xl font-bold pb-2'>PERSONAL INFORMATION :</h1>
                      <p>
                         {" "}
-                        <b> NATIONALITY</b> – Indian <br /> <b> LANGUAGES KNOWN</b> - English[Fluent], Kannada, and Telugu <br />{" "}
+                        <b> NATIONALITY</b> – Indian <br /> <b> LANGUAGES KNOWN</b> - English (Fluent, Kannada, and Telugu <br />{" "}
                         <b>ADDRESS</b> – Bangalore – Karnataka{" "}
                      </p>
                   </div>
                   <div className='py-2 text-center'>
                      <h1 className='text-xl font-bold pb-2'>DECLARATION</h1>
-                     <p>All above-mentioned information is are true to the best of my knowledge and belief.</p>
+                     <p>
+                        I hereby declare that all the information provided in this resume is true and accurate to the best of my knowledge.
+                     </p>
                   </div>
                   <div className='py-2 text-right font-bold'>
                      <h1>[Madan HanumanthaReddy]</h1>
