74279344a58401f32c9d258f09ca14399a54b106
madandot <reddymadan0512@gmail.com>
2025-01-09 18:22:46 +0530

    update

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -21,7 +21,7 @@ const HeroBanner = () => {
             <div className=' px-7 flex md:flex-row flex-col-reverse gap-y-4 md:gap-y-0 justify-center items-center w-full md:h-[90vh] h-[50vh] '>
                <div>
                   <h5 className=' md:font-medium #FFFFFF text-center  md:text-[40px] pt-4 '>
-                     Hi, <span className='text-green-500'>I'm Madan HanumanthaReddy.</span> I'm a designer who codes, driven by a passion
+                     Hi, <span className='text-green-500'>I am Madan HanumanthaReddy.</span> I am a designer who codes, driven by a passion
                      for crafting user-centric and delightful digital experiences that make life simpler for people.
                   </h5>
                </div>
